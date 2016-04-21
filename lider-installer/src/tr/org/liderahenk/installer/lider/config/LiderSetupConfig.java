efe3a2e10380e9131ac0d2b04c108b04132d3c54
canerfyz <caner.feyzullahoglu@agem.com.tr>
2016-04-21 20:03:35 +0300

    bugfix

--- lider-installer/src/tr/org/liderahenk/installer/lider/config/LiderSetupConfig.java
+++ lider-installer/src/tr/org/liderahenk/installer/lider/config/LiderSetupConfig.java
@@ -196,11 +196,6 @@ public class LiderSetupConfig {
 	private String xmppPackageName;
 
 	/**
-	 * Root password for the XMPP server
-	 */
-	private String xmppRootPassword;
-
-	/**
 	 * XMPP server IP
 	 */
 	private String xmppIp;
@@ -784,14 +779,6 @@ public class LiderSetupConfig {
 
 	public void setXmppPackageName(String xmppPackageName) {
 		this.xmppPackageName = xmppPackageName;
-	}
-
-	public String getXmppRootPassword() {
-		return xmppRootPassword;
-	}
-
-	public void setXmppRootPassword(String xmppRootPassword) {
-		this.xmppRootPassword = xmppRootPassword;
 	}
 
 	public String getLdapConfContent() {
