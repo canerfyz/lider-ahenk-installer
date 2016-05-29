8f6dd0f09c5ede34b7f8d79645132c7e097f566d
canerfyz <caner.feyzullahoglu@agem.com.tr>
2016-05-30 01:42:25 +0300

    Preparing default values and new input added to page

--- lider-installer/src/tr/org/liderahenk/installer/lider/wizard/pages/LiderOrganizationPage.java
+++ lider-installer/src/tr/org/liderahenk/installer/lider/wizard/pages/LiderOrganizationPage.java
@@ -17,6 +17,7 @@ public class LiderOrganizationPage extends WizardPage {
 	private LiderSetupConfig config;
 
 	private Text txtOrgCn;
+	private Text txtOrgName;
 
 	public LiderOrganizationPage(LiderSetupConfig config) {
 		super(LiderOrganizationPage.class.getName(), Messages.getString("LIDER_INSTALLATION"), null);
@@ -33,19 +34,26 @@ public class LiderOrganizationPage extends WizardPage {
 
 		Composite cmpOrgCn = GUIHelper.createComposite(mainContainer, new GridLayout(2, false),
 				new GridData(SWT.LEFT, SWT.CENTER, false, false));
+		
+		GUIHelper.createLabel(cmpOrgCn, Messages.getString("ORGANIZATION_NAME"));
+		
+		GridData gd = new GridData();
+		gd.widthHint = 250;
+		
+		txtOrgName = GUIHelper.createText(cmpOrgCn);
+		txtOrgName.setLayoutData(gd);
 
 		GUIHelper.createLabel(cmpOrgCn, Messages.getString("ORGANIZATION_CN"));
 
-		GridData gd = new GridData();
-		gd.widthHint = 250;
 		txtOrgCn = GUIHelper.createText(cmpOrgCn);
 		txtOrgCn.setLayoutData(gd);
+		// TODO add validation from User privilege plugin
 	}
 
 	@Override
 	public IWizardPage getNextPage() {
 		
-		prepareDefaults(txtOrgCn.getText());
+		prepareDefaults(txtOrgName.getText(), txtOrgCn.getText());
 		
 		// Update next page according to selections of this page
 		IWizardPage nextPage = super.getNextPage();
@@ -56,10 +64,26 @@ public class LiderOrganizationPage extends WizardPage {
 		return super.getNextPage();
 	}
 	
-	private void prepareDefaults(String text) {
-		// TODO
-		// TODO
-		// TODO
+	private void prepareDefaults(String orgName, String orgCn) {
+		String[] organizationArr = orgCn.split("\\.");
+		
+		String orgBaseDn = "";
+		String orgBaseCn = "";
+		if (organizationArr.length > 0) {
+			orgBaseCn = organizationArr[0];
+			for (int i = 0; i < organizationArr.length; i++) {
+				if (i != organizationArr.length - 1) {
+					orgBaseDn += "dc=" + organizationArr[i] + ",";
+				} else {
+					orgBaseDn += "dc=" + organizationArr[i];
+				}
+			}
+		}
+		
+		config.setLdapOrgName(orgName);
+		config.setLdapBaseDn(orgBaseDn);
+		config.setLdapOrgCn(orgCn);
+		config.setLdapBaseCn(orgBaseCn);
 	}
 
 }
