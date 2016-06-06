973745e9549f0e9cfe5f35fd43587b6c71ea670e
canerfyz <caner.feyzullahoglu@agem.com.tr>
2016-06-06 19:26:39 +0300

    A simple question page for asking to install a new LDAP server or update an existing one.

---
+++ lider-installer/src/tr/org/liderahenk/installer/lider/wizard/pages/LdapUpdateExistingPage.java
@@ -0,0 +1,52 @@
+package tr.org.liderahenk.installer.lider.wizard.pages;
+
+import org.eclipse.jface.wizard.IWizardPage;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+
+import tr.org.liderahenk.installer.lider.config.LiderSetupConfig;
+import tr.org.liderahenk.installer.lider.i18n.Messages;
+import tr.org.pardus.mys.liderahenksetup.utils.gui.GUIHelper;
+
+public class LdapUpdateExistingPage extends WizardPage implements ILdapPage {
+	
+	private LiderSetupConfig config;
+	
+	private Button btnInstallLdap;
+	
+	public LdapUpdateExistingPage(LiderSetupConfig config) {
+		super(LdapUpdateExistingPage.class.getName(), Messages.getString("LIDER_INSTALLATION"), null);
+		setDescription("3.2 " + Messages.getString("LDAP_UPDATE_OR_INSTALL") + " - "
+				+ Messages.getString("LDAP_UPDATE_OR_INSTALL_DESC"));
+		this.config = config;
+	}
+
+	@Override
+	public void createControl(Composite parent) {
+		
+		Composite compMain = GUIHelper.createComposite(parent, 1);
+		setControl(compMain);
+		
+		Composite compChild = GUIHelper.createComposite(compMain, 1);
+		
+		btnInstallLdap = GUIHelper.createButton(compChild, SWT.RADIO, Messages.getString("INSTALL_LDAP"));
+		btnInstallLdap.setSelection(true);
+		
+		GUIHelper.createButton(compChild, SWT.RADIO, Messages.getString("UPDATE_LDAP"));
+		
+	}
+
+	@Override
+	public IWizardPage getNextPage() {
+		if (btnInstallLdap.getSelection()) {
+			return getWizard().getPage(LdapInstallMethodPage.class.getName()); 
+		} else {
+			System.out.println(LdapUpdateConfPage.class.getName());
+			return getWizard().getPage(LdapUpdateConfPage.class.getName());
+		}
+	}
+	
+	
+}
