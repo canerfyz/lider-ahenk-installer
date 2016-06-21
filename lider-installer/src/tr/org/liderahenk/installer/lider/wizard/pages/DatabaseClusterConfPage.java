ac2c823d915d22ce4fe04c0fa2d11e0cc23b5d14
canerfyz <caner.feyzullahoglu@agem.com.tr>
2016-06-21 19:28:49 +0300

    Cluster configuration improved

--- lider-installer/src/tr/org/liderahenk/installer/lider/wizard/pages/DatabaseClusterConfPage.java
+++ lider-installer/src/tr/org/liderahenk/installer/lider/wizard/pages/DatabaseClusterConfPage.java
@@ -1,10 +1,15 @@
 package tr.org.liderahenk.installer.lider.wizard.pages;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Image;
@@ -20,15 +25,12 @@ import org.eclipse.swt.widgets.Text;
 import tr.org.liderahenk.installer.lider.config.LiderSetupConfig;
 import tr.org.liderahenk.installer.lider.i18n.Messages;
 import tr.org.liderahenk.installer.lider.wizard.model.DatabaseClusterNodeModel;
-import tr.org.pardus.mys.liderahenksetup.constants.NextPageEventType;
 import tr.org.pardus.mys.liderahenksetup.utils.FontProvider;
 import tr.org.pardus.mys.liderahenksetup.utils.gui.GUIHelper;
 
-public class DatabaseClusterConfPage extends WizardPage implements IDatabasePage, ControlNextEvent {
+public class DatabaseClusterConfPage extends WizardPage implements IDatabasePage {
 
 	private LiderSetupConfig config;
-	
-	private NextPageEventType nextPageEventType;
 	
 	private Composite cmpMain;
 	
@@ -39,7 +41,7 @@ public class DatabaseClusterConfPage extends WizardPage implements IDatabasePage
 	private Text txtSstUsername;
 	private Text txtSstPwd;
 	
-	private List<DatabaseClusterNodeModel> nodeList;
+	private Map<Integer, DatabaseClusterNodeModel> nodeMap = new HashMap<Integer, DatabaseClusterNodeModel>();
 	
 	public DatabaseClusterConfPage(LiderSetupConfig config) {
 		super(DatabaseClusterConfPage.class.getName(), Messages.getString("LIDER_INSTALLATION"), null);
@@ -53,7 +55,6 @@ public class DatabaseClusterConfPage extends WizardPage implements IDatabasePage
 		cmpMain = GUIHelper.createComposite(parent, 1);
 		setControl(cmpMain);
 		
-		// TODO add general info label
 		Label lblGeneralInfo = GUIHelper.createLabel(cmpMain, Messages.getString("MARIADB_CLUSTER_GENERAL_INFO"));
 		lblGeneralInfo.setForeground(Display.getCurrent().getSystemColor(SWT.COLOR_DARK_RED));
 		
@@ -65,20 +66,47 @@ public class DatabaseClusterConfPage extends WizardPage implements IDatabasePage
 		txtClusterName = GUIHelper.createText(cmpGeneralInfo);
 		txtClusterName.setText("MariaDB_Cluster");
 		txtClusterName.setMessage(Messages.getString("ENTER_NAME_FOR_CLUSTER"));
+		txtClusterName.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent event) {
+				updatePageCompleteStatus();
+			}
+		});
 
 		GUIHelper.createLabel(cmpGeneralInfo, Messages.getString("MARIA_DB_ROOT_PWD"));
 		txtDbRootPwd = GUIHelper.createText(cmpGeneralInfo);
 		txtDbRootPwd.setMessage(Messages.getString("ENTER_PWD_FOR_DB_ROOT_USER"));
+		txtDbRootPwd.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent event) {
+				updatePageCompleteStatus();
+			}
+		});
 		
 		GUIHelper.createLabel(cmpGeneralInfo, Messages.getString("SST_AUTH_USERNAME"));
 		txtSstUsername = GUIHelper.createText(cmpGeneralInfo);
 		txtSstUsername.setText("sst_user");
 		txtSstUsername.setMessage(Messages.getString("ENTER_USERNAME_FOR_SST_USER"));
-
+		txtSstUsername.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent event) {
+				updatePageCompleteStatus();
+			}
+		});
+		
 		GUIHelper.createLabel(cmpGeneralInfo, Messages.getString("SST_AUTH_PWD"));
 		txtSstPwd = GUIHelper.createText(cmpGeneralInfo); 
 		txtSstPwd.setMessage(Messages.getString("ENTER_PWD_FOR_SST_USER"));
-
+		txtSstPwd.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent event) {
+				updatePageCompleteStatus();
+			}
+		});
+		
+		Label lblNodeInfo = GUIHelper.createLabel(cmpMain, Messages.getString("MARIADB_CLUSTER_NODE_INFO"));
+		lblNodeInfo.setForeground(Display.getCurrent().getSystemColor(SWT.COLOR_DARK_RED));
+		
 		// Labels for headers
 		Composite cmpLabels = GUIHelper.createComposite(cmpMain, 4);
 		cmpLabels.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
@@ -135,6 +163,8 @@ public class DatabaseClusterConfPage extends WizardPage implements IDatabasePage
 		});
 		
 		redraw();
+		
+		updatePageCompleteStatus();
 	}
 	
 	private void handleRemoveButtonClick(SelectionEvent event) {
@@ -144,6 +174,11 @@ public class DatabaseClusterConfPage extends WizardPage implements IDatabasePage
 		if (children != null) {
 			for (int i = 0; i < children.length; i++) {
 				if (children[i].equals(btnThis) && i - 1 > 0) {
+					Group group = (Group) children[i - 1];
+					Control[] childrenOfGroup = group.getChildren();
+					Label number = (Label) childrenOfGroup[0];
+					Integer intNumber = Integer.parseInt(number.getText());
+					nodeMap.remove(intNumber);
 					children[i - 1].dispose();
 					children[i].dispose();
 					redraw();
@@ -151,10 +186,12 @@ public class DatabaseClusterConfPage extends WizardPage implements IDatabasePage
 				}
 			}
 		}
+		
+		updatePageCompleteStatus();
 	}
 	
 	private void createNewNode(Composite cmpNodeList) {
-		Group grpClusterNode = GUIHelper.createGroup(cmpNodeList, 5);
+		Group grpClusterNode = GUIHelper.createGroup(cmpNodeList, 6);
 		grpClusterNode.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
 		
 		GridData gd = new GridData();
@@ -162,19 +199,40 @@ public class DatabaseClusterConfPage extends WizardPage implements IDatabasePage
 		
 		DatabaseClusterNodeModel clusterNode = new DatabaseClusterNodeModel();
 		
+		Integer nodeNumber = nodeMap.size() + 1;
+		GUIHelper.createLabel(grpClusterNode, nodeNumber.toString());
+		
 		Text txtNodeIp = GUIHelper.createText(grpClusterNode);
 		txtNodeIp.setLayoutData(gd);
 		txtNodeIp.setMessage(Messages.getString("ENTER_IP_FOR_THIS_NODE"));
+		txtNodeIp.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent event) {
+				updatePageCompleteStatus();
+			}
+		});
 		clusterNode.setTxtNodeIp(txtNodeIp);
 		
 		Text txtNodeName = GUIHelper.createText(grpClusterNode);
 		txtNodeName.setLayoutData(gd);
 		txtNodeName.setMessage(Messages.getString("ENTER_NAME_FOR_THIS_NODE"));
+		txtNodeName.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent event) {
+				updatePageCompleteStatus();
+			}
+		});
 		clusterNode.setTxtNodeName(txtNodeName);
 		
 		Text txtNodeRootPwd = GUIHelper.createText(grpClusterNode);
 		txtNodeRootPwd.setLayoutData(gd);
 		txtNodeRootPwd.setMessage("ENTER_ROOT_PWD_OF_THIS_NODE");
+		txtNodeRootPwd.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent event) {
+				updatePageCompleteStatus();
+			}
+		});
 		clusterNode.setTxtNodeRootPwd(txtNodeRootPwd);
 		
 		Button btnNodeNewSetup = new Button(grpClusterNode, SWT.CHECK | SWT.BORDER);
@@ -183,22 +241,12 @@ public class DatabaseClusterConfPage extends WizardPage implements IDatabasePage
 		btnNodeNewSetup.setToolTipText(Messages.getString("UNCHECK_IF_THIS_NODE_IS_ALREADY_INSTALLED"));
 		clusterNode.setBtnNodeNewSetup(btnNodeNewSetup);
 		
-		nodeList.add(clusterNode);
+		nodeMap.put(nodeNumber, clusterNode);
 	}
 	
 	private void redraw() {
 		cmpMain.redraw();
 		cmpMain.layout(true, true);
-	}
-
-	@Override
-	public NextPageEventType getNextPageEventType() {
-		return this.nextPageEventType;
-	}
-
-	@Override
-	public void setNextPageEventType(NextPageEventType nextPageEventType) {
-		this.nextPageEventType = nextPageEventType;
 	}
 
 	@Override
@@ -212,22 +260,61 @@ public class DatabaseClusterConfPage extends WizardPage implements IDatabasePage
 	private void setConfigVariables() {
 		
 		config.setDatabaseClusterAddress(createWsrepClusterAddress());
+		config.setDatabaseClusterName(txtClusterName.getText());
+		config.setDatabaseRootPassword(txtDbRootPwd.getText());
+		config.setDatabaseSstUsername(txtSstUsername.getText());
+		config.setDatabaseSstPwd(txtSstPwd.getText());
+		config.setDatabaseNodeMap(nodeMap);
 	}
 
 	private String createWsrepClusterAddress() {
 		
 		String wsrepClusterAddress = "";
 		
-		for (DatabaseClusterNodeModel clusterNode : nodeList) {
+		for (Iterator<Entry<Integer, DatabaseClusterNodeModel>> iterator = nodeMap.entrySet().iterator(); iterator.hasNext();) {
+			Entry<Integer, DatabaseClusterNodeModel> entry = iterator.next();
+			DatabaseClusterNodeModel clusterNode = entry.getValue();
 			wsrepClusterAddress += clusterNode.getTxtNodeIp().getText() + ",";
+			
 		}
-
+		
 		// Delete last comma
 		wsrepClusterAddress = wsrepClusterAddress.substring(0, wsrepClusterAddress.length()-1);
 
 		return wsrepClusterAddress;
 		
 	}
+	
+	private void updatePageCompleteStatus() {
+		
+		boolean pageComplete = false;
+		
+		if (!txtClusterName.getText().isEmpty() &&
+				!txtDbRootPwd.getText().isEmpty() &&
+				!txtSstUsername.getText().isEmpty() &&
+				!txtSstPwd.getText().isEmpty()) {
+			for (Iterator<Entry<Integer, DatabaseClusterNodeModel>> iterator = nodeMap.entrySet().iterator(); iterator.hasNext();) {
+				Entry<Integer, DatabaseClusterNodeModel> entry = iterator.next();
+				DatabaseClusterNodeModel node = entry.getValue();
+				if (!node.getTxtNodeIp().getText().isEmpty() &&
+						!node.getTxtNodeName().getText().isEmpty() && 
+						!node.getTxtNodeRootPwd().getText().isEmpty() &&
+						node.getBtnNodeNewSetup().getSelection()) {
+					pageComplete = true;
+				} else {
+					pageComplete = false;
+					break;
+				}
+				
+			}
+			
+			setPageComplete(pageComplete);
+		} else {
+			setPageComplete(false);
+		}
+	}
+	
+	
 	
 	
 	
