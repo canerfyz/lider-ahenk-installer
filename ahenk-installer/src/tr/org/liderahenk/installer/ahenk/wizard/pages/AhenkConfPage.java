a3d5ac3510f44e30c6722b95f4dbb913f0c40b45
canerfyz <caner.feyzullahoglu@agem.com.tr>
2016-08-29 19:28:59 +0300

    Write to file method moved to SetupUtils

--- ahenk-installer/src/tr/org/liderahenk/installer/ahenk/wizard/pages/AhenkConfPage.java
+++ ahenk-installer/src/tr/org/liderahenk/installer/ahenk/wizard/pages/AhenkConfPage.java
@@ -1,9 +1,6 @@
 package tr.org.liderahenk.installer.ahenk.wizard.pages;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -214,7 +211,7 @@ public class AhenkConfPage extends WizardPage implements ControlNextEvent {
 
 		text = SetupUtils.replace(map, text);
 		config.setAhenkConfContent(text);
-		config.setAhenkAbsPathConfFile(writeToFile(text, "ahenk.conf"));
+		config.setAhenkAbsPathConfFile(SetupUtils.writeToFileReturnPath(text, "ahenk.conf"));
 		return confPage;
 	}
 
@@ -260,37 +257,6 @@ public class AhenkConfPage extends WizardPage implements ControlNextEvent {
 				e.printStackTrace();
 			}
 		}
-	}
-
-	/**
-	 * Creates file under temporary file directory and writes configuration to
-	 * it. Returns absolute path of created temp file.
-	 * 
-	 * @param content
-	 * @param namePrefix
-	 * @param nameSuffix
-	 * @return absolute path of created temp file
-	 */
-	private String writeToFile(String content, String fileName) {
-
-		String absPath = null;
-
-		try {
-			File temp = new File(System.getProperty("java.io.tmpdir") + "/" + fileName);
-
-			FileWriter fileWriter = new FileWriter(temp.getAbsoluteFile());
-
-			BufferedWriter buffWriter = new BufferedWriter(fileWriter);
-
-			buffWriter.write(content);
-			buffWriter.close();
-
-			absPath = temp.getAbsolutePath();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return absPath;
 	}
 
 	private void updatePageCompleteStatus() {
