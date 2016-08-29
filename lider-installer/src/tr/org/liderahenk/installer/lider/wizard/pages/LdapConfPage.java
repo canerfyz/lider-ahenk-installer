8f6f5e34401a083ffdf12e7598cbc532479360d4
canerfyz <caner.feyzullahoglu@agem.com.tr>
2016-08-29 19:29:49 +0300

    Write to file method moved to SetupUtils

--- lider-installer/src/tr/org/liderahenk/installer/lider/wizard/pages/LdapConfPage.java
+++ lider-installer/src/tr/org/liderahenk/installer/lider/wizard/pages/LdapConfPage.java
@@ -1,9 +1,6 @@
 package tr.org.liderahenk.installer.lider.wizard.pages;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -254,7 +251,7 @@ public class LdapConfPage extends WizardPage implements ILdapPage, ControlNextEv
 
 		// Set config variables before going to next page
 		config.setLdapConfContent(text);
-		config.setLdapAbsPathConfFile(writeToFile(text, "ldapconfig"));
+		config.setLdapAbsPathConfFile(SetupUtils.writeToFileReturnPath(text, "ldapconfig"));
 		
 		config.setLdapBaseDn(this.baseDn.getText());
 		config.setLdapAdminCn(this.adminCn.getText());
@@ -320,37 +317,6 @@ public class LdapConfPage extends WizardPage implements ILdapPage, ControlNextEv
 		}
 	}
 
-	/**
-	 * Creates file under temporary file directory and writes configuration to
-	 * it. Returns absolute path of created temp file.
-	 * 
-	 * @param content
-	 * @param namePrefix
-	 * @param nameSuffix
-	 * @return absolute path of created temp file
-	 */
-	private String writeToFile(String content, String fileName) {
-
-		String absPath = null;
-
-		try {
-			File temp = new File(System.getProperty("java.io.tmpdir") + File.separator + fileName);
-
-			FileWriter fileWriter = new FileWriter(temp.getAbsoluteFile());
-
-			BufferedWriter buffWriter = new BufferedWriter(fileWriter);
-
-			buffWriter.write(content);
-			buffWriter.close();
-
-			absPath = temp.getAbsolutePath();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return absPath;
-	}
-	
 	public NextPageEventType getNextPageEventType() {
 		return nextPageEventType;
 	}
