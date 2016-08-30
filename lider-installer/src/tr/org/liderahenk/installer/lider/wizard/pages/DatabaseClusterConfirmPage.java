2f2b247bf66c049483ddd45e20e80082b8b9f478
canerfyz <caner.feyzullahoglu@agem.com.tr>
2016-08-30 18:10:50 +0300

    Installation information improved

--- lider-installer/src/tr/org/liderahenk/installer/lider/wizard/pages/DatabaseClusterConfirmPage.java
+++ lider-installer/src/tr/org/liderahenk/installer/lider/wizard/pages/DatabaseClusterConfirmPage.java
@@ -1,13 +1,18 @@
 package tr.org.liderahenk.installer.lider.wizard.pages;
 
+import java.util.Iterator;
+import java.util.Map.Entry;
+
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
 
 import tr.org.liderahenk.installer.lider.config.LiderSetupConfig;
 import tr.org.liderahenk.installer.lider.i18n.Messages;
+import tr.org.liderahenk.installer.lider.wizard.model.DatabaseNodeInfoModel;
 import tr.org.pardus.mys.liderahenksetup.constants.NextPageEventType;
 import tr.org.pardus.mys.liderahenksetup.utils.gui.GUIHelper;
 
@@ -18,8 +23,8 @@ import tr.org.pardus.mys.liderahenksetup.utils.gui.GUIHelper;
 public class DatabaseClusterConfirmPage extends WizardPage implements IDatabasePage {
 	private LiderSetupConfig config;
 
-	private Label lblIp;
-
+	private StyledText ipTextArea;
+	
 	public DatabaseClusterConfirmPage(LiderSetupConfig config) {
 		super(DatabaseClusterConfirmPage.class.getName(), Messages.getString("LIDER_INSTALLATION"), null);
 		setDescription("2.3 " + Messages.getString("MARIA_DB_CLUSTER_CONFIRM"));
@@ -32,12 +37,18 @@ public class DatabaseClusterConfirmPage extends WizardPage implements IDatabaseP
 		Composite container = GUIHelper.createComposite(parent, 1);
 		setControl(container);
 
-		GridData gd = new GridData();
-		gd.widthHint = 700;
-		gd.minimumWidth = 200;
-		lblIp = GUIHelper.createLabel(container, "localhost");
-		lblIp.setLayoutData(gd);
+		// IP list label
+		GUIHelper.createLabel(container, Messages.getString("MACHINES_THAT_DB_CLUSTER_WILL_BE_INSTALLED"));
+		// Add a text area for IP list
+		ipTextArea = new StyledText(container, SWT.MULTI | SWT.BORDER | SWT.WRAP | SWT.V_SCROLL);
 
+		GridData txtAreaGd = new GridData(SWT.FILL, SWT.FILL, true, false);
+		txtAreaGd.heightHint = 100;
+
+		ipTextArea.setEditable(false);
+		ipTextArea.setLayoutData(txtAreaGd);
+		
+		
 		GUIHelper.createLabel(container, "- " + Messages.getString("USE_DEFAULT_REPOSITORY"));
 
 		GUIHelper.createLabel(container, Messages.getString("MARIA_DB_CLUSTER_WILL_BE_INSTALLED") + " "
@@ -46,9 +57,20 @@ public class DatabaseClusterConfirmPage extends WizardPage implements IDatabaseP
 
 	@Override
 	public IWizardPage getNextPage() {
-		// Set the IP info in the opening of page
-		lblIp.setText("- IP: " + config.getDatabaseClusterAddress());
 
+		// Set the IP info in the opening of page
+		String allIps = "";
+		for (Iterator<Entry<Integer, DatabaseNodeInfoModel>> iterator = config.getDatabaseNodeInfoMap()
+				.entrySet().iterator(); iterator.hasNext();) {
+
+			Entry<Integer, DatabaseNodeInfoModel> entry = iterator.next();
+			final DatabaseNodeInfoModel clusterNode = entry.getValue();
+
+			allIps += "-" + clusterNode.getNodeIp() + "\n";
+			
+		}
+		ipTextArea.setText(allIps);
+		
 		((ControlNextEvent) super.getNextPage()).setNextPageEventType(NextPageEventType.CLICK_FROM_PREV_PAGE);
 
 		// Set page complete to true, otherwise it does not go into getNextPage
